9778a9b20a73b60174ce676924c4f60411d7bb84
poke5352-ok <64702792+poke5352-ok@users.noreply.github.com>
2020-06-15 12:17:35 -0700

    Added faq

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import json
 
 app = Flask(__name__)
 
@@ -10,6 +10,15 @@ def index():
 @app.route('/viewer')
 def viewer():
     return render_template('viewer.html')
+
+@app.route('/faq')
+def faq():
+    with open('./data/faq.json', encoding="utf8") as faq_json:
+        faq = json.load(faq_json)
+        return render_template('faq.html', faq=faq)
+    
+
+
 
 
 
